3f7f0285ca2fcc68e5b6470fc1e851e6394b9aba
Renato Alves Soares <renatoguara2020@gmail.com>
2023-02-06 15:46:23 -0300

    ok

--- lastInsertedID.php
+++ lastInsertedID.php
@@ -8,7 +8,7 @@ try {
   $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-  $sql = "INSERT INTO MyGuests (firstname, lastname, email) VALUES ('John', 'Doe', 'john@example.com')";
+  $stmt = $conn->prepare("INSERT INTO MyGuests (firstname, lastname, email) VALUES ('John', 'Doe', 'john@example.com')");
   // use exec() because no results are returned
   $conn->exec($sql);
   $last_id = $conn->lastInsertId();
